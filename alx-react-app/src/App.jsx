e147796d41eea7cddc74d6fb2394245ce6023668
Mohanad <mohanad5ayman@gmail.com>
2026-01-10 19:08:35 +0200

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,7 +1,7 @@
 import Header from './Header.jsx'
 import MainContent from './MainContent.jsx'
 import Footer from './Footer.jsx'
-import {UserProfile} from './components/UserProfile.jsx'
+import UserProfile from './components/UserProfile.jsx'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
