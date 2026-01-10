5d4e2c0f7f7a78a571e359dafaabf3a26aecfc4e
Mohanad <mohanad5ayman@gmail.com>
2026-01-10 19:00:08 +0200

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,11 +1,12 @@
+import Header from './Header.jsx'
+import MainContent from './MainContent.jsx'
+import Footer from './Footer.jsx'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage.jsx'
-import Header from './Header.jsx'
-import MainContent from './MainContent.jsx'
-import Footer from './Footer.jsx'
+
 
 function App() {
   const [count, setCount] = useState(0)
