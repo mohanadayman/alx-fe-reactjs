243aacd5b4ab7f51e3602b6f7fbd71041c2315db
Mohanad <mohanad5ayman@gmail.com>
2026-01-10 19:07:48 +0200

    add UserProfile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './Header.jsx'
 import MainContent from './MainContent.jsx'
 import Footer from './Footer.jsx'
+import {UserProfile} from './components/UserProfile.jsx'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -37,6 +38,7 @@ function App() {
 
       <Header />
       <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" /> 
       <Footer />
     </>
   )
