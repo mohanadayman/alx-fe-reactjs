a3747440a317468a0f460848c88d774fb8922909
Mohanad <mohanad5ayman@gmail.com>
2026-01-10 19:14:23 +0200

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,12 +1,12 @@
-import Header from './Header'
-import MainContent from './MainContent'
-import Footer from './Footer'
-import UserProfile from './components/UserProfile'
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import WelcomeMessage from './components/WelcomeMessage'
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
+import UserProfile from './components/UserProfile';
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import WelcomeMessage from './components/WelcomeMessage';
 
 
 function App() {
@@ -35,7 +35,6 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <WelcomeMessage />
-
       <Header />
       <MainContent />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" /> 
