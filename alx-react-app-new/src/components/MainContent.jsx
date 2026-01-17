7a82326d271948f6c37808f0254c3238ef18e81f
Mohanad <mohanad5ayman@gmail.com>
2026-01-18 00:36:28 +0200

    css

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,6 +1,6 @@
 function MainContent() {
     return (
-        <main>
+        <main style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
             <p>I love to visit New York, Paris, and Tokyo.</p>
         </main>
     );
