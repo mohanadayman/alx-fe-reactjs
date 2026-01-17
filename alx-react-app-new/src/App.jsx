484b8d92126465fa7bf39c2c7a266f9eac6857d8
Mohanad <mohanad5ayman@gmail.com>
2026-01-18 00:41:07 +0200

    Counter

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,7 +7,7 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-
+import Counter from './components/Counter';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -34,6 +34,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Counter />
       <WelcomeMessage />
       <Header />
       <MainContent />
