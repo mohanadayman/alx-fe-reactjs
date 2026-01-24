af5335a2e8f8c300da07ecf1b1980d33104c1fb1
Mohanad <mohanad5ayman@gmail.com>
2026-01-24 16:19:44 +0200

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,5 +1,5 @@
-import AddRecipeForm from './components/AddRecipeForm.jsx'
-import RecipeList from './components/RecipeList.jsx'
+import { AddRecipeForm } from './components/AddRecipeForm.jsx'
+import { RecipeList } from './components/RecipeList.jsx'
 import './App.css'
 
 function App() {
