61b90751bfb16bce273e6a3977c2753813000fd1
Mohanad <mohanad5ayman@gmail.com>
2026-01-24 16:12:08 +0200

    recipe

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,14 @@
+import AddRecipeForm from './components/AddRecipeForm.jsx'
+import RecipeList from './components/RecipeList.jsx'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <AddRecipeForm />
+      <RecipeList />
+    </div>
+  )
+}
+
+export default App
